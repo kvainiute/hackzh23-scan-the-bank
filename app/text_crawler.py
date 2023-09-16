0265484a254bebec57bf038f9952f4e13665390c
Kamile Vainiute <kamile@igem.org>
2023-09-16 20:51:58 +0200

    Direct & Indirect data

--- app/text_crawler.py
+++ app/text_crawler.py
@@ -24,42 +24,24 @@ def classify(file_content):
     
     # email regex
     email_regex = r"[\w\.-]+@[\w\.-]+"
-    hasEmail = bool(re.search(email_regex, file_content))
+    isDirect = bool(re.search(email_regex, file_content))
+
+    # full name and company NER
+
+    if not isDirect:
+        resNer = ner(file_content)
+        return checkNer(resNer)
+
     
     # IBAN regex
     iban_regex = r"[A-Z]{2}[0-9]{2}(?:[ ]?[0-9]{4}){4}(?!(?:[ ]?[0-9]){3})(?:[ ]?[0-9]{1,2})"
-    hasIBAN = bool(re.search(iban_regex, file_content))
-    if hasIBAN and hasEmail:
-        return True
+    isIndirect = bool(re.search(iban_regex, file_content))
 
-    # return "review"
+    if not isIndirect:
+        # Phone number regex
+        phone_number_regex = r"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"
+        isIndirect = bool(re.search(phone_number_regex, file_content))
 
-
-    # full name NER
-
-    resNer = ner(file_content)
-    return checkNer(resNer)
-
-
-
-
-
-    # full name
-    fullName = question_answerer(
-        question = "What is the person's full name?",
-        context = file_content
-    )
-    hasFullName = fullName['score'] > 0.5
-    return hasFullName or hasEmail
-
-    model_name = "bert-base-uncased"
-    model = BertForTokenClassification.from_pretrained(model_name)
-    tokenizer = BertTokenizer.from_pretrained(model_name)
-
-    tokenized_text = tokenizer.tokenize(file_content)
-
-    inputs = tokenizer(tokenized_text, padding=True, truncation=True, return_tensors="pt")
-    outputs = model(**inputs)
-
-    logits = outputs.logits
-    predictions = torch.argmax(logits, dim=2)
+    if isDirect and isIndirect:
+        return 'True'
+    return 'False'
